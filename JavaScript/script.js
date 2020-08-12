b50b8698c837f09a7bc854a1a435bdfc6277520b
Eric0Zhang <silentstorm321f@qq.com>
2020-08-12 18:40:08 +0800

    UPDATE

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,20 +2,21 @@ const list = document.createElement('ul');
 const info = document.createElement('p');
 const html = document.querySelector('html');
 
-info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+// 字符集必须用UTF8才能正常使用中文，用GB2312没用。
+info.textContent = '这是一个动态列表。点击列表添加一列，点击已存在的一列修改内容。';
 
 document.body.appendChild(info);
 document.body.appendChild(list);
 
-html.onclick = function() {
+list.onclick = function () {
   const listItem = document.createElement('li');
-  const listContent = prompt('What content do you want the list item to have?');
+  const listContent = prompt('您要创建的条目内容是什么?');
   listItem.textContent = listContent;
   list.appendChild(listItem);
 
-  listItem.onclick = function(e) {
+  listItem.onclick = function (e) {
     e.stopPropagation();
-    const listContent = prompt('Enter new content for your list item');
+    const listContent = prompt('您要将条目内容改成什么？');
     this.textContent = listContent;
   }
 }
